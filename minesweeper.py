9bb472c0a0df7dc5f2ad9f0a94d7e4f10516efad
Bill Nguyen <ivecleandedmyroom@gmail.com>
2024-02-27 11:17:10 +1100

    fixed the writing of scores to files

--- minesweeper.py
+++ minesweeper.py
@@ -84,7 +84,7 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        while j >= 0 and arr[j] > key:  # Adjusted comparison for reverse sorting order
+        while j >= 0 and arr[j] < key:  # Adjusted comparison for reverse sorting order
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
